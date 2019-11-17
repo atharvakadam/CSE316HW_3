96d690c1561d8c9d4416cc3563839e729ab2376e
atharvakadam <atharvakadam@gmail.com>
2019-11-17 14:21:23 -0500

    Designed the modal

--- src/components/list_screen/ListModalYesNo.js
+++ src/components/list_screen/ListModalYesNo.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 export class ListModalYesNo extends Component {
     render() {
         return (
-            <div>
-                
+            <div id="modal_background" >
+                    <div id="modal_yes_no_dialog" >
+                        {this.props.children}
+                        <p>Delete list?</p>
+                        <strong>Are you sure you want to delete this list?</strong>
+                        <p></p>
+                        <div id="yes_no_buttons">
+                            <button id="yesButton" onClick={this.removeListandGoHome}>Yes</button>
+                            <button id="noButton" onClick={this.props.closeModal}>No</button>    
+                        </div>
+                        <p>This List will not be retrievable</p>
+                    </div>
             </div>
         )
     }
